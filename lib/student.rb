9214f4fda088bb7a32542dc42ad21557945a329d
amanda johnson <manda.panda91@yahoo.com>
2018-03-14 16:03:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -80,7 +80,7 @@ class Student
     end
   end
   
-  def self.first_x_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT TOP x FROM students WHERE grade = 10 
     SQL
